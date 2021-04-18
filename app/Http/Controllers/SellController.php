347fabb69f871bc23b72d9216b871198f076f441
kr95656 <katmsg8239@gmail.com>
2021-04-18 11:52:44 +0900

    商品カテゴリの追加

--- app/Http/Controllers/SellController.php
+++ app/Http/Controllers/SellController.php
@@ -3,16 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\ItemCondition;
+use App\Models\PrimaryCategory;
 use Illuminate\Http\Request;
 
-// seederを使用するいみとは？
 
 class SellController extends Controller
 {
     public function showSoldItems()
     {
         // クエリを発行
+        $categories = PrimaryCategory::orderBy('sort_no')->get();
         $conditons = ItemCondition::orderBy('sort_no')->get();
-        return view('sell')->with('conditions', $conditons);
+        return view('sell')
+                ->with('conditions', $conditons)
+                ->with('categories', $categories);
     }
 }
--- resources/views/sell.blade.php
+++ resources/views/sell.blade.php
@@ -64,7 +64,17 @@
                     <div class="form-group mt-3">
                         <label for="category">カテゴリ</label>
                         <select name="category" class="custom-select form-control @error('category') is-invalid @enderror">
-                            {{-- 次のパートで実装します --}}
+                            @foreach ($categories as $category)
+                            {{--  大カテゴリ  --}}
+                                <optgroup label="{{ $category->name }}">
+                                    @foreach ($category->secondaryCategories as $secondary_category)
+                                    {{--  小カテゴリ  --}}
+                                        <option value="{{$secondary_category->id}}" {{ old('category') === $secondary_category->id ? "selected":""}}>
+                                            {{$secondary_category->name}}
+                                        </option>
+                                    @endforeach
+                                </optgroup>
+                            @endforeach
                         </select>
                         @error('category')
                         <span class="invalid-feedback" role="alert">
@@ -77,7 +87,6 @@
                     <div class="form-group mt-3">
                         <label for="condition">商品の状態</label>
                         <select name="condition" class="custom-select form-control @error('condition') is-invalid @enderror">
-                            {{-- 次のパートで実装します --}}
                             @foreach ($conditions as $condition)
                                 <option value="{{$condition->id}}" {{old('condition') == $condition->id ? 'selected' : ''}}>
                                     {{$condition->name}}
